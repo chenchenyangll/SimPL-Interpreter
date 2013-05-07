8154b6457affe379801d1f70185e48f48b728bd4
RusonWong <wangchun0109@126.com>
2013-05-07 20:36:14 +0800

    bugfix：list无法识别的bug

--- src/edu/sjtu/simpl/ui/Console.java
+++ src/edu/sjtu/simpl/ui/Console.java
@@ -9,22 +9,21 @@ public class Console {
 	  System.out.println("Reading from standard input...");
 	  //System.out.print("Enter an expression like \"1+(2+3)*var;\" :");
 	  new SimPL(System.in);
-	  try
-	  {
-		while(true)
-		{
-			System.out.println("SimPL> ");
-			SimpleNode n = SimPL.Program();
-			n.dump("");
-			System.out.println("-------");
+	 
+		while (true) {
 			SimPL.ReInit(System.in);
+			try {
+				System.out.print("SimPL> ");
+				SimpleNode n = SimPL.Program();
+				n.dump("");
+				System.out.println("-------");
+				
+			} catch (Exception e) {
+				System.out.println("Syntax Error!");
+				System.out.println(e.getMessage());
+			}
 		}
-	  }
-	  catch (Exception e)
-	  {
-	    System.out.println("Oops.");
-	    System.out.println(e.getMessage());
-	  }
+
 	}
 }
 
