df75531c4a0063cee9662e8508ce45d90cf3bfb8
RusonWong <wangchun0109@126.com>
2013-05-07 20:15:15 +0800

    新版本

--- src/edu/sjtu/simpl/ui/Console.java
+++ src/edu/sjtu/simpl/ui/Console.java
@@ -1,7 +1,31 @@
 package edu.sjtu.simpl.ui;
 
+import edu.sjtu.simpl.grammar.SimPL;
+import edu.sjtu.simpl.grammar.SimpleNode;
+
 public class Console {
-	public static void main(String[] args) {
-		
+	public static void main(String args [])
+	{
+	  System.out.println("Reading from standard input...");
+	  //System.out.print("Enter an expression like \"1+(2+3)*var;\" :");
+	  new SimPL(System.in);
+	  try
+	  {
+		while(true)
+		{
+			System.out.println("SimPL> ");
+			SimpleNode n = SimPL.Program();
+			n.dump("");
+			System.out.println("-------");
+			SimPL.ReInit(System.in);
+		}
+	  }
+	  catch (Exception e)
+	  {
+	    System.out.println("Oops.");
+	    System.out.println(e.getMessage());
+	  }
 	}
 }
+
+
